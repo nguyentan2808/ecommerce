99c0f9a9f6a6a07a31eb8b14b4561e22be266261
nguyentan2808 <tanpro01635147801@gmail.com>
2022-01-27 23:47:59 +0700

    chore-fe: extend chakra-ui teal color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // Extend Chakra-ui color
+                teal: {
+                    50: "#E6FFFA",
+                    100: "#B2F5EA",
+                    200: "#81E6D9",
+                    300: "#4FD1C5",
+                    400: "#38B2AC",
+                    500: "#319795",
+                    600: "#2C7A7B",
+                    700: "#285E61",
+                    800: "#234E52",
+                    900: "#1D4044",
+                },
+            },
+        },
     },
     plugins: [],
     darkMode: "class",
